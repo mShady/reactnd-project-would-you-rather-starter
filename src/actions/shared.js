673961d57ff3e5bd8fb1141174433ac86bd4a362
Shady M. Najib <smostafa@integrant.com>
2019-10-05 19:38:52 +0200

    Dispatch initial data (questions)

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,13 +1,14 @@
 import { getInitialData } from "../utils/api";
 import { receiveUsers } from "../actions/users";
+import { receiveQuestions } from "../actions/questions";
 import { showLoading, hideLoading } from "react-redux-loading";
 
 export function handleInitialData() {
   return dispatch => {
     dispatch(showLoading());
-    return getInitialData().then(({ users }) => {
+    return getInitialData().then(({ users, questions }) => {
       dispatch(receiveUsers(users));
-      // TODO: recieve questions
+      dispatch(receiveQuestions(questions));
       dispatch(hideLoading());
     });
   };
