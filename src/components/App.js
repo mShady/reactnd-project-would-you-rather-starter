6d71cfa5ae1e89c5522e98cc00f6c031f934acb5
Shady M. Najib <smostafa@integrant.com>
2019-10-11 16:47:34 +0200

    Add routing and skeleton components to test it

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,14 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import { connect } from "react-redux";
 import { handleInitialData } from "../actions/shared";
 import LoadingBar from "react-redux-loading";
 import Login from "./Login";
 import Home from "./Home";
+import Question from "./Question";
+import NewQuestion from "./NewQuestion";
+import Leaderboard from "./Leaderboard";
+import NotFound from "./NotFound";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 class App extends Component {
   componentDidMount() {
@@ -11,10 +16,26 @@ class App extends Component {
   }
   render() {
     return (
-      <div>
-        <LoadingBar />
-        {this.props.needAuth ? <Login /> : <Home />}
-      </div>
+      <BrowserRouter>
+        <Fragment>
+          <LoadingBar />
+          {this.props.needAuth ? (
+            <Login />
+          ) : (
+            <Switch>
+              <Route path="/" exact component={Home} />
+              <Route
+                path="/questions/:question_id"
+                exact
+                component={Question}
+              />
+              <Route path="/add" exact component={NewQuestion} />
+              <Route path="/leaderboard" exact component={Leaderboard} />
+              <Route component={NotFound} />
+            </Switch>
+          )}
+        </Fragment>
+      </BrowserRouter>
     );
   }
 }
---
+++ src/components/Question.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+
+class Question extends Component {
+  render() {
+    return <div>Question!</div>;
+  }
+}
+export default Question;
