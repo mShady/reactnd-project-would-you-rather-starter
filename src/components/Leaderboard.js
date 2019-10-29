03c265f9baf5500a3fee29a3cdf07e4c65593b4a
Shady M. Najib <shady@comcal.org>
2019-10-29 23:47:25 +0200

    Add Leaderboard component

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,8 +1,42 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 
 class Leaderboard extends Component {
   render() {
-    return <div>Leaderboard!</div>;
+    return (
+      <div>
+        {this.props.orderedUsers.map(user => {
+          const answersCount = Object.keys(user.answers).length;
+          const score = answersCount + user.questions.length;
+          return (
+            <div key={user.id} className="itemContainer">
+              <div>{user.name}</div>
+              <img
+                src={user.avatarURL}
+                alt={`Avatar of ${user.name}`}
+                className="avatar"
+              />
+              <div>Answered Questions: {answersCount}</div>
+              <div>Created Questions: {user.questions.length}</div>
+              <div>Score: {score}</div>
+            </div>
+          );
+        })}
+      </div>
+    );
   }
 }
-export default Leaderboard;
+
+function mapStateToProps({ users }) {
+  const orderedUsers = Object.keys(users)
+    .sort(
+      (a, b) =>
+        users[b].questions.length +
+        Object.keys(users[b].answers).length -
+        (users[a].questions.length + Object.keys(users[a].answers).length)
+    )
+    .map(id => users[id]);
+  return { orderedUsers };
+}
+
+export default connect(mapStateToProps)(Leaderboard);
