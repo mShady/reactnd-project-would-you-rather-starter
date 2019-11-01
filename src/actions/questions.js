ea54863f20961b8f7c769e38a15f014621051b94
Shady M. Najib <shady@comcal.org>
2019-11-01 19:47:05 +0200

    Add "New Question" actions

--- src/actions/questions.js
+++ src/actions/questions.js
@@ -1,9 +1,10 @@
 import { showLoading, hideLoading } from "react-redux-loading";
-import { saveAnswer } from "../utils/api";
-import { addUserAnswer } from "./users";
+import { saveAnswer, saveQuestion } from "../utils/api";
+import { addUserAnswer, addUserQuestion } from "./users";
 
 export const RECEIVE_QUESTIONS = "RECEIVE_QUESTIONS";
 export const ANSWER_QUESTION = "ANSWER_QUESTION";
+export const ADD_QUESTION = "ADD_QUESTION";
 
 export function receiveQuestions(questions) {
   return {
@@ -19,12 +20,35 @@ export function answerQuestion(questionId, selectedOption, authedUser) {
   };
 }
 
+export function addQuestion(question) {
+  return {
+    type: ADD_QUESTION,
+    question
+  };
+}
+
 export function handleAnswerQuestion(questionId, selectedOption, authedUser) {
   return dispatch => {
     dispatch(showLoading());
     return saveAnswer(authedUser, questionId, selectedOption).then(() => {
       dispatch(answerQuestion(questionId, selectedOption, authedUser));
       dispatch(addUserAnswer(authedUser, questionId, selectedOption));
+      dispatch(hideLoading());
+    });
+  };
+}
+
+export function handleAddQuestion(optionOneText, optionTwoText) {
+  return (dispatch, getState) => {
+    const { authedUser } = getState();
+    dispatch(showLoading());
+    return saveQuestion({
+      optionOneText,
+      optionTwoText,
+      author: authedUser
+    }).then(question => {
+      dispatch(addQuestion(question));
+      dispatch(addUserQuestion(authedUser, question.id));
       dispatch(hideLoading());
     });
   };
--- src/actions/users.js
+++ src/actions/users.js
@@ -1,5 +1,6 @@
 export const RECEIVE_USERS = "RECEIVE_USERS";
 export const ADD_USER_ANSWER = "ADD_USER_ANSWER";
+export const ADD_USER_QUESTION = "ADD_USER_QUESTION";
 
 export function receiveUsers(users) {
   return {
@@ -12,5 +13,12 @@ export function addUserAnswer(userId, questionId, selectedAnswer) {
   return {
     type: ADD_USER_ANSWER,
     info: { userId, questionId, selectedAnswer }
+  };
+}
+
+export function addUserQuestion(userId, questionId) {
+  return {
+    type: ADD_USER_QUESTION,
+    info: { userId, questionId }
   };
 }
